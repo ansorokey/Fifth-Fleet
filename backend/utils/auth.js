2f316d2e6aa16d09ac41f21b5ee280cf8749c28a
ansorokey <ansorokey@gmail.com>
2023-09-12 15:01:27 -0400

    added first auth helper function

---
+++ backend/utils/auth.js
@@ -0,0 +1,34 @@
+// Authentication helper functions
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+// Sets the JWT token after the user logs in or signs up
+// JWT contains user id, username, and email
+const setTokenCookie = (res, user) => {
+    // Create the token.
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    const token = jwt.sign(
+      { data: safeUser },
+      secret,
+      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    );
+
+    const isProduction = process.env.NODE_ENV === "production";
+
+    // Set the token cookie
+    res.cookie('token', token, {
+      maxAge: expiresIn * 1000, // maxAge in milliseconds
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction && "Lax"
+    });
+
+    return token;
+  };
