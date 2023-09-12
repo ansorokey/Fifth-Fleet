699e1fadb4fec561f82fa88d4f5b96fde7c0735d
ansorokey <ansorokey@gmail.com>
2023-09-12 14:10:09 -0400

    error handling middleware added

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 // Routers
 const indexRouter = require('./routes');
@@ -51,5 +52,42 @@ app.use(
 
 // Use router
 app.use(indexRouter);
+
+// Error handling
+// Middleware that creates a 404 error if no matching route is found
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Middleware to catch Validation errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Final error handler
+// Returns a formatted error message in JSON
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
