ce2347907707588a3cdd86e2c2cf11ba01852c99
ansorokey <ansorokey@gmail.com>
2023-09-14 10:13:40 -0400

    added sign up validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,11 +2,34 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+// Validate User sign up information
+const validateSignup = [
+    check('email')
+        .exists({values: 'falsy'})
+        .isEmail()
+        .withMessage('Please provide a valid email'),
+    check('username')
+        .exists({values: 'falsy'})
+        .isLength({min: 4})
+        .withMessage('Please provide a username 4 and 30 characters in length'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email address'),
+    check('password')
+        .exists({values: 'falsy'})
+        .isLength({min: 6})
+        .withMessage('Password must be at least 6 characters'),
+    handleValidationErrors
+]
 
 // Signing Up
-router.post('/', async (req, res, next) => {
+// Validates user signup info before creating user
+router.post('/', validateSignup, async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password);
 
