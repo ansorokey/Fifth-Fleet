04e682347267fa00d0ce1d340605def69e4d60ba
ansorokey <ansorokey@gmail.com>
2023-09-21 22:20:01 -0400

    reset ws port to 5000

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { createServer } = require('http');
 const WebSocket = require('ws');
 
 // the websocket port will listen to a different port
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 // Routers
 const indexRouter = require('./routes');
