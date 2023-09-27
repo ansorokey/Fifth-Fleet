ec4866237ed7aba7522f9ab633228ff3164467ca
ansorokey <ansorokey@gmail.com>
2023-09-27 18:13:07 -0400

    changed src of profile pic in PlayerListing comp

--- frontend/src/components/PlayerListing/index.js
+++ frontend/src/components/PlayerListing/index.js
@@ -3,7 +3,7 @@ import './PlayerListing.css';
 function PlayerListing({user}) {
     return (<div className='player-list-ctn'>
         <div className='player-list-pics'>
-            <img className='player-pic-icon' src='https://i.redd.it/4al31h1ehd431.png'></img>
+            <img className='player-pic-icon' src={user?.avatarUrl}></img>
             <img className='player-weapon-icon' src={user?.Weapon?.iconUrl} />
         </div>
         {user?.username}
