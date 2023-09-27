6b1417ae7e1cf06c56f71e6d6b06d2bb8a147ebf
ansorokey <ansorokey@gmail.com>
2023-09-27 18:32:09 -0400

    added dispatch to profile page

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,8 +1,21 @@
 import './ProfilePage.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 function ProfilePage({userId}) {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.userProfile);
+
+    useEffect(() => {
+        dispatch(loadFullUser(userId));
+    }, []);
+
     return (<div>
         <h1>This is user {userId}'s profile page</h1>
+        <div className='profile-header'>
+            <img src='' />
+            <img src={user?.avatarUrl}/>
+        </div>
     </div>);
 }
 
