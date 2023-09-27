e1cd7fed8980bd998d38a8649de5b3538ae9eafe
ansorokey <ansorokey@gmail.com>
2023-09-27 19:19:33 -0400

    added route for creating comments

--- backend/routes/api/guildphotos.js
+++ backend/routes/api/guildphotos.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const { GuildPhoto } = require('../../db/models');
+const { GuildPhoto, Comment } = require('../../db/models');
 
 const router = express.Router();
+
+
+// create a comment for a guild photo
+router.post('/:photoId/comments', async (req, res) => {
+    const {photoId} = req.params;
+    const userId = req.user.id;
+    const {content} = req.body;
+
+    const newComment = await Comment.create({
+        userId,
+        photoId,
+        content
+    });
+
+    return res.json({
+        comment: newComment
+    });
+
+});
 
 // Get all guild photos across all guilds
 router.get('/all', async (req, res) => {
